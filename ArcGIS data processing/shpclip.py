dd4675a6757ed0701be5461fbe014eec2111f4be
HaoJun-Zhang99 <58284056+HaoJun-Zhang99@users.noreply.github.com>
2025-05-22 00:05:08 +0800

    Update shpclip.py

--- ArcGIS data processing/shpclip.py
+++ ArcGIS data processing/shpclip.py
@@ -7,8 +7,8 @@ output_shp = "D:\TREESLab\TREES\S-A-T\dataset1\sea_level\Patagonian"
 archi_shp = "D:\TREESLab\TREES\S-A-T\dataset1\currents\Patagonian.shp"
 for shp in shps:
     catchment_name = shp[:-1]
-    # 输出文件名称
+    # name of output file
     out_feature_class = output_shp + catchment_name
-    # 调用ArcGIS操作
+    # operate with Arcgis function
     arcpy.Clip_analysis(shp, archi_shp, out_feature_class, "")
     print "finished"
